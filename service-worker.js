8c9f20da188724023dcdec2d454c21b51200c179
Corey White <realityhacker@gmail.com>
2026-01-15 19:59:53 -0500

    Implement service worker for caching files

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "psypher-cache-v1";
+const FILES_TO_CACHE = [
+  "/Correspondence/psypher.html",
+  "/Correspondence/manifest.json",
+  "/Correspondence/service-worker.js"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
